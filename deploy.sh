164912248bcb96c9a6f3122fe10a85eae4af0245
sonpd2 <55070883+sonpd2@users.noreply.github.com>
2021-02-04 14:12:38 +0700

    check has been deployed zsh to avoid multi message telegram
    check has been deployed zsh to avoid multi message telegram

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ cat <<EOF > /etc/profile.d/telegram-alert.sh
 # Log connections
 bash $ALERTSCRIPT_PATH bash
 EOF
+echo "Bash done"
 }
 
 add_zsh () {
@@ -14,7 +15,9 @@ cat <<EOF >> /etc/zsh/zshrc
 # Log connections
 bash $ALERTSCRIPT_PATH zsh
 EOF
+echo "ZSH done"
 }
+
 
 ALERTSCRIPT_PATH="/opt/ssh-login-alert-telegram/alert.sh"
 
@@ -22,11 +25,16 @@ echo "Deploying alerts..."
 add_profiled
 
 echo "Check if ZSH is installed.."
-
+$file_zsh="/etc/zsh/zshrc"
 HAS_ZSH=$(grep -o -m 1 "zsh" /etc/shells)
 if [ ! -z $HAS_ZSH ]; then
     echo "ZSH is installed, deploy alerts to zshrc"
+    if grep -q "$ALERTSCRIPT_PATH" "$file"; ##note the space after the string you are searching for
+    then
+    echo "ZSH has been deployed"
+    else
     add_zsh
+    fi
 else
     echo "No zsh detected"
 fi
